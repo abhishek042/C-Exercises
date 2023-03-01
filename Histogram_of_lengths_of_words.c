e36ceb9769ba5174e718a6939aedd666bf947747
Abhishek Singh <47527051+abhishek042@users.noreply.github.com>
2023-03-01 15:17:33 +0530

    Add files via upload

---
+++ Histogram_of_lengths_of_words.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+int main()
+{
+    int wc=26,nc=10;
+    int warr[wc],narr[10];
+    return 0;
+}
---
+++ ex1_19.c
@@ -0,0 +1,37 @@
+#include<stdio.h>
+#define MAXSTRING 1000
+
+void reverse(char s[])
+{
+    int i=0,j;
+    char rev[MAXSTRING];
+    while(s[i++]!='\0');
+    j=i-1;
+    i=0;
+    while(j>=0)
+        rev[i++]=s[j--];
+    i++;
+    rev[i]='\0';
+    j=0;
+    while(j<i)
+        {
+            printf("%c",rev[j]);
+            j++;
+        }
+    printf("%s\n",rev);
+}
+int main()
+{
+    reverse("HELLO abhi");
+    char string[MAXSTRING];
+    int c,i=0;
+    while((c=getchar())!=EOF)
+    {
+        string[i++]=c;
+        if(c=='\n'){
+            string[i]='\0';
+            reverse(string);
+        }
+    }
+    return 0;
+}
